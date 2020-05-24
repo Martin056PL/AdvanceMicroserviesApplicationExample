110ad10cb70d780ffc43d27c385a34f5691e030e
Martin056PL <kamil.wawer@pollub.edu.pl>
2020-05-24 21:30:51 +0200

    Add clean project of discovery server

---
+++ discovery-server-eureka/src/main/java/wawer/kamil/discoveryservereureka/DiscoveryServerEurekaApplication.java
@@ -0,0 +1,13 @@
+package wawer.kamil.discoveryservereureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerEurekaApplication.class, args);
+    }
+
+}
---
+++ discovery-server-eureka/src/test/java/wawer/kamil/discoveryservereureka/DiscoveryServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package wawer.kamil.discoveryservereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
